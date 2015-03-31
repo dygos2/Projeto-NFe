d7e53b3f799f4c6fe0eae15433de180c82c1be02
Rodrigo Serzedello <dygos2@gmail.com>
2015-03-31 09:52:01 -0700

    Inclusao de novo WS de contingencia do RS

--- FN4EnvioCtl/My Project/Settings.Designer.vb
+++ FN4EnvioCtl/My Project/Settings.Designer.vb
@@ -143,6 +143,16 @@ Namespace My
                 Return CType(Me("FN4EnvioCtl_NFe_ConsultaServicos1_NfeStatusServico2"),String)
             End Get
         End Property
+        
+        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.WebServiceUrl),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("https://nfe.sefazvirtual.rs.gov.br/ws/NfeStatusServico/NfeStatusServico2.asmx")>  _
+        Public ReadOnly Property FN4EnvioCtl_NFe_ConsultaServicosRS_NfeStatusServico2() As String
+            Get
+                Return CType(Me("FN4EnvioCtl_NFe_ConsultaServicosRS_NfeStatusServico2"),String)
+            End Get
+        End Property
     End Class
 End Namespace
 
